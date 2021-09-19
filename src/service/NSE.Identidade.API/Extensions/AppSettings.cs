99da5f4a5a97031e9e4ef6c5fb7739b6e7617dab
Jalter <jalter_roberto@hotmail.com>
2021-09-19 19:38:15 -0300

    develop custom base controller

--- src/service/NSE.Identidade.API/Extensions/AppSettings.cs
+++ src/service/NSE.Identidade.API/Extensions/AppSettings.cs
@@ -8,7 +8,7 @@ namespace NSE.Identidade.API.Extensions
     public class AppSettings
     {
         public string Secret { get; set; }
-        public int ExpiracaoHora { get; set; }
+        public int ExpiracaoHoras { get; set; }
         public string Emissor { get; set; }
         public string ValidoEm { get; set; }
     }
