feb6104a6de7cad2b75e029a76dbe0f706fed2f6
jalter <jalter_rs@yahoo.com.br>
2021-10-12 21:51:19 -0300

    commit all

--- src/service/NSE.Identidade.API/Data/ApplicationDbContext.cs
+++ src/service/NSE.Identidade.API/Data/ApplicationDbContext.cs
@@ -1,13 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace NSE.Identidade.API.Data
 {
-    public class ApplicationDbContext: IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
